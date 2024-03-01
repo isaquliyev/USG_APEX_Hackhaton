02595ecf60bf2e9554dd5736c948c3f2edee1932
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-01 15:53:39 +0400

    Simple Profile and Home page design

--- mobile_app/lib/Presentation/Screens/BottomNavWidgets/home.dart
+++ mobile_app/lib/Presentation/Screens/BottomNavWidgets/home.dart
@@ -5,6 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("This is home page");
+    return ListView.separated(itemBuilder: (context, index) {
+      return Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text("Name of Illness"),
+                Text("#1515"),
+                SizedBox(height: 20,),
+                Text("Mualice basladi"),
+                Text("Mualice Bitdi"),
+              ],
+            ),
+            Image.asset('images/ali_hasanli.png', height: 100, width: 100,)
+          ],
+        ),
+      );
+    },
+      itemCount: 5,
+      scrollDirection: Axis.vertical,
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider();
+      },
+    );
+
   }
 }
--- mobile_app/lib/Presentation/Screens/BottomNavWidgets/profile.dart
+++ mobile_app/lib/Presentation/Screens/BottomNavWidgets/profile.dart
@@ -5,6 +5,62 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('This is profile Page.');
+    return Container(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/ali_hasanli.png'), // Add your image asset
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Ali Hasanli',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              enabled: false,
+              decoration: InputDecoration(
+                labelText: "lorem@ipsum.com",
+                labelStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                disabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade600)
+                ),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              enabled: false,
+              decoration: InputDecoration(
+                labelText: "+994 999 99 99",
+                labelStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade600)
+                ),
+                prefixIcon: Icon(Icons.phone),
+              ),
+            ),
+          ],
+      ),
+    );
   }
 }
