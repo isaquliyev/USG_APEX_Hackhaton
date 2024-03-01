ab3daf9bf507d45186a2e5cc4c6abc179e6cd5de
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-01 19:21:28 +0400

    Analysis page and download part

--- mobile_app/lib/Presentation/Screens/illness_detail_page.dart
+++ mobile_app/lib/Presentation/Screens/illness_detail_page.dart
@@ -1,9 +1,21 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:mobile_app/Data/ReceitDetail.dart';
 
-class IllnessDetailPage extends StatelessWidget {
-  IllnessDetailPage({super.key});
+class IllnessDetailPage extends StatefulWidget {
+  const IllnessDetailPage({super.key});
+
+  @override
+  State<IllnessDetailPage> createState() => _IllnessDetailPageState();
+}
+
+class _IllnessDetailPageState extends State<IllnessDetailPage> {
   MedicalReceiptWidget medicalReceiptWidget = MedicalReceiptWidget(
     patientName: 'John Doe',
     dateOfBirth: 'January 1, 1980',
@@ -21,6 +33,23 @@ class IllnessDetailPage extends StatelessWidget {
     totalAmount: 100.0,
     paymentMethod: 'Credit Card',
   );
+
+  bool isReceiptActive = true;
+  bool isAnalysisActive = false;
+
+  Future<bool> _downloadImage() async {
+    // Replace 'assets/your_image.jpg' with the actual path to your image asset
+    final ByteData data = await rootBundle.load('images/analysis1.png');
+    List<int> bytes = data.buffer.asUint8List();
+    final result = await ImageGallerySaver.saveImage(Uint8List.fromList(bytes));
+
+    if (result['isSuccess']) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
@@ -31,7 +60,10 @@ class IllnessDetailPage extends StatelessWidget {
               children: [
                 Text(
                   "Cholera",
-                  style: Theme.of(context).textTheme.displayLarge,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .displayLarge,
                 ),
                 SizedBox(
                   height: 20,
@@ -40,7 +72,10 @@ class IllnessDetailPage extends StatelessWidget {
                   padding: EdgeInsets.symmetric(horizontal: 24.0),
                   child: Text(
                     "Doctor's notes: The patient is advised to follow the prescribed treatment, maintain strict hygiene practices, and refrain from work or school until declared fit by a medical professional. For any further information, please feel free to contact our office.",
-                    style: Theme.of(context).textTheme.displayMedium,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .displayMedium,
                   ),
                 ),
                 SizedBox(
@@ -48,31 +83,55 @@ class IllnessDetailPage extends StatelessWidget {
                 ),
                 Row(
                   children: [
-                    Expanded(
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          this.isReceiptActive = true;
+                          this.isAnalysisActive = false;
+                        });
+                      },
                       child: Container(
                         height: 40,
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width / 2,
                         decoration: BoxDecoration(
+                          color:
+                          this.isReceiptActive ? Colors.white : Colors.grey,
                           borderRadius:
-                              BorderRadius.only(topLeft: Radius.circular(16)),
+                          BorderRadius.only(topLeft: Radius.circular(16)),
                         ),
-                        child: Icon(Icons.receipt,),
+                        child: Icon(
+                          Icons.receipt,
+                        ),
                       ),
                     ),
-                    Expanded(
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          this.isReceiptActive = false;
+                          this.isAnalysisActive = true;
+                        });
+                      },
                       child: Container(
                         height: 40,
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width / 2,
                         decoration: BoxDecoration(
-                          color: Colors.grey,
+                          color: isAnalysisActive ? Colors.white : Colors.grey,
                           borderRadius:
-                              BorderRadius.only(topRight: Radius.circular(16)),
+                          BorderRadius.only(topRight: Radius.circular(16)),
                         ),
                         child: Icon(Icons.analytics),
                       ),
                     ),
                   ],
                 ),
-
-                Padding(
+                this.isReceiptActive
+                    ? Padding(
                   padding: EdgeInsets.symmetric(horizontal: 20),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -90,9 +149,11 @@ class IllnessDetailPage extends StatelessWidget {
                       Text(
                         'Patient Information:\n'
                             'Name: ${medicalReceiptWidget.patientName}\n'
-                            'Date of Birth: ${medicalReceiptWidget.dateOfBirth}\n'
+                            'Date of Birth: ${medicalReceiptWidget
+                            .dateOfBirth}\n'
                             'Address: ${medicalReceiptWidget.patientAddress}\n'
-                            'Phone Number: ${medicalReceiptWidget.phoneNumber}\n'
+                            'Phone Number: ${medicalReceiptWidget
+                            .phoneNumber}\n'
                             'Email: ${medicalReceiptWidget.email}\n',
                       ),
                       Divider(),
@@ -102,29 +163,34 @@ class IllnessDetailPage extends StatelessWidget {
                             'Date: ${medicalReceiptWidget.dateOfConsultation}\n'
                             'Time: ${medicalReceiptWidget.timeOfConsultation}\n'
                             'Doctor: ${medicalReceiptWidget.doctorName}\n'
-                            'Clinic/Hospital: ${medicalReceiptWidget.clinicName}\n'
+                            'Clinic/Hospital: ${medicalReceiptWidget
+                            .clinicName}\n'
                             'Address: ${medicalReceiptWidget.clinicAddress}\n',
                       ),
-        Divider(),
+                      Divider(),
                       // Services Rendered
                       Text(
                         'Services Rendered:\n'
-                            'Description: ${medicalReceiptWidget.servicesDescription}\n\n',
+                            'Description: ${medicalReceiptWidget
+                            .servicesDescription}\n\n',
                       ),
-Divider(),
+                      Divider(),
                       // Fees and Payments
                       Text(
-                        'Consultation Fee: \$${medicalReceiptWidget.consultationFee}\n'
-                            'Medication Cost: \$${medicalReceiptWidget.medicationCost}\n'
-                            'Total Amount: \$${medicalReceiptWidget.totalAmount}\n\n',
+                        'Consultation Fee: \$${medicalReceiptWidget
+                            .consultationFee}\n'
+                            'Medication Cost: \$${medicalReceiptWidget
+                            .medicationCost}\n'
+                            'Total Amount: \$${medicalReceiptWidget
+                            .totalAmount}\n\n',
                       ),
-Divider(),
+                      Divider(),
                       // Payment Method
                       Text(
                         'Payment Method:\n'
                             '${medicalReceiptWidget.paymentMethod}\n\n',
                       ),
-Divider(),
+                      Divider(),
                       // Footer
                       Text(
                         'Thank you for choosing our services. Please feel free to contact us for any further assistance.\n'
@@ -132,6 +198,30 @@ Divider(),
                         style: TextStyle(fontStyle: FontStyle.italic),
                       ),
                     ],
+                  ),
+                )
+                    : Container(
+                  height: 800,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset('images/analysis1.png', height: 400, width: 300, fit: BoxFit.fill,),
+                        GestureDetector(
+                          onTap: () async {
+                            if (await _downloadImage()) {
+                              Fluttertoast.showToast(msg: "Download Succesfully");
+                            }
+                            else {
+                              Fluttertoast.showToast(msg: "Download failed");
+                            }
+                          },
+                            child: Icon(Icons.download_for_offline, size: 70, color: Colors.grey, )),
+                      ],
+                    );
+                  },
+                    itemCount: 4,
                   ),
                 ),
               ],
