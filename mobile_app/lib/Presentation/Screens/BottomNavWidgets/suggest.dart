6bdd1d4ae864a513d0315b982ebffbc8a90af5eb
isaquliyev <isa.quluyev.wip@gmail.com>
2024-03-01 20:52:29 +0400

    File upload complate partly

--- mobile_app/lib/Presentation/Screens/BottomNavWidgets/suggest.dart
+++ mobile_app/lib/Presentation/Screens/BottomNavWidgets/suggest.dart
@@ -1,83 +1,166 @@
 import 'dart:io';
 
+import 'package:dio/dio.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:iconsax/iconsax.dart';
+
 
 class SuggestPage extends StatefulWidget {
-  const SuggestPage({super.key});
+  const SuggestPage({ Key? key }) : super(key: key);
 
   @override
-  State<SuggestPage> createState() => _SuggestPageState();
+  _SuggestPageState createState() => _SuggestPageState();
 }
 
-class _SuggestPageState extends State<SuggestPage> {
-  bool isDocsDownload = false;
+class _SuggestPageState extends State<SuggestPage> with SingleTickerProviderStateMixin {
   late AnimationController loadingController;
+
   File? _file;
   PlatformFile? _platformFile;
 
   selectFile() async {
     final file = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['png', 'jpg', 'pdf'],
+        type: FileType.custom,
+        allowedExtensions: ['png', 'jpg', 'jpeg']
     );
 
     if (file != null) {
-      _file = File(file.files.single.path!);
-      _platformFile = file.files.first;
+      setState(() {
+        _file = File(file.files.single.path!);
+        _platformFile = file.files.first;
+      });
     }
 
     loadingController.forward();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: isDocsDownload
-          ? CircularPercentIndicator(
-              radius: 120.0,
-              lineWidth: 16.0,
-              percent: 0.4,
-              animation: true,
-              animationDuration: 2000,
-              center: const Text(
-                "7400 AZN",
-                style: TextStyle(fontSize: 25),
-              ),
-              progressColor: Colors.green,
-            )
-          : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "File daxil edin:",
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  GestureDetector(
-                    onTap: selectFile,
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(50)),
-                      child: GestureDetector(
-                        onTap: () {
+  void initState() {
+    loadingController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..addListener(() { setState(() {}); });
 
-                        },
-                        child: Icon(
-                          Icons.upload_file,
-                          size: 60,
-                        ),
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 100,),
+            SizedBox(height: 50,),
+            Text('Upload your file', style: TextStyle(fontSize: 25, color: Colors.grey.shade800, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            Text('File should be jpg, png', style: TextStyle(fontSize: 15, color: Colors.grey.shade500),),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: selectFile,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: Radius.circular(10),
+                    dashPattern: [10, 4],
+                    strokeCap: StrokeCap.round,
+                    color: Colors.blue.shade400,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          color: Colors.blue.shade50.withOpacity(.3),
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Iconsax.folder_open, color: Colors.blue, size: 40,),
+                          SizedBox(height: 15,),
+                          Text('Select your file', style: TextStyle(fontSize: 15, color: Colors.grey.shade400),),
+                        ],
                       ),
                     ),
-                  ),
-                ],
+                  )
               ),
             ),
+            _platformFile != null
+                ? Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Selected File',
+                      style: TextStyle(color: Colors.grey.shade400, fontSize: 15, ),),
+                    SizedBox(height: 10,),
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.shade200,
+                                offset: Offset(0, 1),
+                                blurRadius: 3,
+                                spreadRadius: 2,
+                              )
+                            ]
+                        ),
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.file(_file!, width: 70,)
+                            ),
+                            SizedBox(width: 10,),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(_platformFile!.name,
+                                    style: TextStyle(fontSize: 13, color: Colors.black),),
+                                  SizedBox(height: 5,),
+                                  Text('${(_platformFile!.size / 1024).ceil()} KB',
+                                    style: TextStyle(fontSize: 13, color: Colors.grey.shade500),
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Container(
+                                      height: 5,
+                                      clipBehavior: Clip.hardEdge,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        color: Colors.blue.shade50,
+                                      ),
+                                      child: LinearProgressIndicator(
+                                        value: loadingController.value,
+                                      )
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                          ],
+                        )
+                    ),
+                    SizedBox(height: 20,),
+                    // MaterialButton(
+                    //   minWidth: double.infinity,
+                    //   height: 45,
+                    //   onPressed: () {},
+                    //   color: Colors.black,
+                    //   child: Text('Upload', style: TextStyle(color: Colors.white),),
+                    // )
+                  ],
+                ))
+                : Container(),
+            SizedBox(height: 150,),
+          ],
+        ),
+      ),
     );
   }
 }
