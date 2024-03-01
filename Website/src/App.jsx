9cf61e45a361492f5b2725afe5a30c71bdb0eded
alihasanli <ahasanli639.dev@gmail.com>
2024-03-01 18:13:01 +0400

    routing, adding css of components

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './style.css'
 import DoctorDashboard from './components/doctorDashboard/pages/DoctorDashboard'
+import Patients from './components/doctorDashboard/pages/patients/Patients'
 
 const App = () => {
   return (
     <>
       <DoctorDashboard />
+      {/* <Routes>
+        <Route path="/doctordashboard" element={<DoctorDashboard />} />
+        <Route path="/doctordashboard/patients" element={<Patients />} />
+      </Routes> */}
     </>
   )
 }
