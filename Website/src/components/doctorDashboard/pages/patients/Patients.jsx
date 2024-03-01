c980929dc0355e3ef4983715cbf485ac4f9daa73
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2024-03-01 19:48:27 +0400

    changes

--- Website/src/components/doctorDashboard/pages/patients/Patients.jsx
+++ Website/src/components/doctorDashboard/pages/patients/Patients.jsx
@@ -1,9 +1,11 @@
+
 import style from './patients.module.css'
+import Statistics from './statistics/Statistics'
 
 const Patients = () => {
   return (
     <div className={style.container}>
-      adsaasd
+      <Statistics />
     </div>
   )
 }
