68b05796d003482b9b52a922ec2d4d3dac87542e
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2024-03-01 21:00:54 +0400

    last change

--- Website/src/components/doctorDashboard/pages/search/Search.jsx
+++ Website/src/components/doctorDashboard/pages/search/Search.jsx
@@ -1,10 +1,21 @@
+import { GoMail } from "react-icons/go"
+import { FaRegBell } from "react-icons/fa"
+import { RiSearchLine } from "react-icons/ri"
 import style from './search.module.css'
 
 const Search = () => {
   return (
     <div className={style.container}>
-      
-    </div>
+      <h2 className={style.title}>Dashboard</h2>
+      <div className={style.search_div}>
+        <GoMail className={style.icon} />
+        <FaRegBell className={style.icon} />
+        <div>
+          <RiSearchLine />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+    </div>  
   )
 }
 
